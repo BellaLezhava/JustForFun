f1fce8c3f1b50807995486412e6bf6d1bfcf72d3
bella.lezhava@icloud.com <bella.lezhava@icloud.com>
2023-03-04 22:39:40 +0400

    Add project files.

---
+++ JustForMe/Program.cs
@@ -0,0 +1,38 @@
+﻿class JustForFun
+{
+    static void Main(string[] args)
+    {
+        RunCalculateCurrency();
+    }
+    static decimal CalculateNbgRate(decimal amount, string threeLetterRegionCode)
+    {
+        decimal rate = 0.00M;
+        switch (threeLetterRegionCode)
+        {
+            case "USD": //US Dollar
+                rate = 2.6064M;
+                break;
+            case "EUR": //Europian Union
+                rate = 2.7680M;
+                break;
+        }
+        return amount * rate;
+    }
+    static void RunCalculateCurrency()
+    {
+        Console.Write("Enter an amount you want to exchange: ");
+        string amountForExchange = Console.ReadLine();
+        Console.Write("Enter a three-letter region code: ");
+        string region = Console.ReadLine();
+        if (decimal.TryParse(amountForExchange, out decimal amount))
+        {
+            decimal amountInGel = CalculateNbgRate(amount, region);
+            Console.WriteLine("You have " + amountInGel + " in GEL");
+        }
+        else
+        {
+            Console.WriteLine("You did not enter a valid region!");
+        }
+    }
+
+}
