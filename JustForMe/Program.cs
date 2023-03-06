5dd56e1a222caa539f26af6fbb35c919a65446f7
bella.lezhava@icloud.com <bella.lezhava@icloud.com>
2023-03-06 22:02:05 +0400

    Update

--- JustForMe/Program.cs
+++ JustForMe/Program.cs
@@ -1,38 +1,52 @@
-﻿class ExchangeRate
+﻿class FromOrdinalToCardinal
 {
+    //რიცხვების დაწერა რიგითობითი მნიშვნელობით
     static void Main(string[] args)
     {
-        RunCalculateCurrency();
-    }
-    static decimal CalculateNbgRate(decimal amount, string threeLetterRegionCode)
-    {
-        decimal rate = 0.00M;
-        switch (threeLetterRegionCode)
-        {
-            case "USD": //US Dollar
-                rate = 2.6064M;
-                break;
-            case "EUR": //Europian Union
-                rate = 2.7680M;
-                break;
-        }
-        return amount * rate;
-    }
-    static void RunCalculateCurrency()
-    {
-        Console.Write("Enter an amount you want to exchange: ");
-        string amountForExchange = Console.ReadLine();
-        Console.Write("Enter a three-letter region code: ");
-        string region = Console.ReadLine();
-        if (decimal.TryParse(amountForExchange, out decimal amount))
-        {
-            decimal amountInGel = CalculateNbgRate(amount, region);
-            Console.WriteLine("You have " + amountInGel + " in GEL");
-        }
-        else
-        {
-            Console.WriteLine("You did not enter a valid region!");
-        }
-    }
+        RunCardinalToOrdinal();
 
+    }
+    //ლოგიკური ოპერაცია, რომელიც რიცხვებისთვის დაბოლობებს არჩევს 
+    static string CardinalToOrdinal (int number)
+        {
+        switch (number)
+        {
+            case 11:
+            case 12:
+            case 13:
+                return number + "th";
+            default:
+                string numberAsText = number.ToString();
+                char lastDigit = numberAsText[numberAsText.Length - 1];
+                string suffix = string.Empty;
+                /*ყველა დანარჩენი რიცხვის დასასრული თუ მათი ბოლო რიცხვია 1 ბოლოს მიუწეროს "st",2 ბოლოს მიუწეროს "nd",3 ბოლოს მიუწეროს "rd", 
+                 * ხოლო ყველა დანარჩენს ბოლოს მიუწეროს "th", დააბრუნოს რიცხვითი მნიშვნელობა + დასასრული.
+                 */
+                switch (lastDigit)
+                {
+                    case '1':
+                        suffix = "st";
+                        break;
+                    case '2':
+                        suffix = "nd";
+                        break;
+                    case '3':
+                        suffix = "rd";
+                        break;
+                    default:
+                        suffix = "th";
+                        break;
+                }
+                return number + suffix;
+            }
+        }
+    //გამოიგანს იმდენ რიცხვით მნიშვნელობას რამდენსაც მიუთითებ
+    static void RunCardinalToOrdinal()
+    {
+        for (int number = 1; number <= 30; number++)
+        {
+            Console.Write(CardinalToOrdinal(number));
+            Console.WriteLine();
+        }
+    }
 }
